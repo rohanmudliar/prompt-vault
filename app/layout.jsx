72866fdacc58190f1129633871def68098ae0d70
rohanm <rohan.mudliar@gmail.com>
2023-10-23 14:47:23 +0530

    project structure

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Prompt Vault",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html>
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
